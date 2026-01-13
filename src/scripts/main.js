c8136cb733a8f8b75ec084a9b7c4b40099a53550
KJOrwin <kjorwin@gmail.com>
2026-01-13 17:26:31 +0000

    Moved oscillator out of the function, added exercise logic, playNote function now controls the gain

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,5 @@
 let notes = {
-    "s": ["C", 261.63, "note-c"],
+    "s": ["&#x2193;C", 261.63, "note-c"],
     "e": ["C#", 277.18, "note-c#"],
     "d": ["D", 293.66, "note-d"],
     "r": ["Eb", 311.13, "note-eb"],
@@ -11,43 +11,87 @@ let notes = {
     "j": ["A", 440, "note-a"],
     "i": ["Bb", 466.16, "note-bb"],
     "k": ["B", 493.88, "note-b"],
-    "l": ["C", 523.25, "note-C"]
+    "l": ["&#x2191;C", 523.25, "note-C"]
 };
 
 const ctx = new AudioContext();
-let osc = null;
+const osc = ctx.createOscillator();
+const gain = ctx.createGain()
+const fadeTime = 0.05;
+osc.type = "triangle";
+osc.frequency.value = 261.63;
+gain.gain.value = 0;
+osc.connect(gain).connect(ctx.destination);
+osc.start();
 
-function playNote(frequency) {
-    osc = ctx.createOscillator();
-    osc.frequency.value = frequency;
-    osc.connect(ctx.destination);
-    osc.start()
-}
+let randomNote = ""
+let randomNotes = [];
+let currentNote = 0;
 
-//let notes_text = document.getElementById("notes");
+let output = document.getElementById("notes");
+let successText = document.getElementById("success");
+
 let selected_key = "";
 let keyPressed = "";
+let failed = false;
+
+function startExercise() {
+    currentNote = 0;
+    output.innerHTML = ""
+    successText.innerHTML = ""
+    failed = false;
+    for (i = 0; i < 4; i++) {
+        randomNote = Object.keys(notes)[Math.floor(Math.random() * Object.keys(notes).length)]
+        randomNotes[i] = randomNote;
+        output.innerHTML += notes[randomNote][0] + ", ";
+    }
+}
+
+function playNote(gainValue, frequencyValue = -1) {
+    if (frequencyValue != -1) {
+        osc.frequency.value = frequencyValue;
+    }
+    gain.gain.cancelScheduledValues(ctx.currentTime);
+    gain.gain.setValueAtTime(gain.gain.value, ctx.currentTime);
+    gain.gain.linearRampToValueAtTime(gainValue, ctx.currentTime + fadeTime);
+}
+
+document.addEventListener("keydown", () => ctx.resume(), { once: true });
 
 document.addEventListener("keydown", (event) => {
     if (notes[event.key] != undefined && !keyPressed) {
-        //notes_text.innerHTML += notes[event.key][0] + "<br>";
         selected_key = event.key
         keyPressed = window.getComputedStyle(document.getElementById(notes[event.key][2])).backgroundColor;
-        document.getElementById(notes[event.key][2]).style.backgroundColor = "#ffaaaa";
-        playNote(notes[event.key][1]);
+        document.getElementById(notes[event.key][2]).style.backgroundColor = "#c0c0c0";
+        playNote(1, notes[event.key][1]);
+
+        if (failed == false && randomNotes[0]) {
+            if (event.key == randomNotes[currentNote]) {
+                successText.innerHTML += "Success <br/>";
+            } else {
+                successText.innerHTML += "Fail <br/>";
+                successText.innerHTML += "<br/>You Lose!<br/>";
+                failed = true;
+            }
+
+            if (currentNote == 3 && failed == false) {
+                successText.innerHTML += "<br/>You Win!<br/>";
+            }
+            currentNote++;
+        }
+        
     }
 });
 
 document.addEventListener("keyup", (event) => {
     if (event.key == selected_key) {
-        osc.stop();
+        playNote(0);
         if (keyPressed == "rgb(255, 255, 255)") {
             document.getElementById(notes[event.key][2]).style.backgroundColor = "#ffffff";
         } else if (keyPressed == "rgb(0, 0, 0)") {
             document.getElementById(notes[event.key][2]).style.backgroundColor = "#000000";
         }
         
-        osc = null;
         keyPressed = "";
     }
 });
