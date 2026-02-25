af85f64f1e704094bd848c387c2ca7f33c0c20e7
KJOrwin <kjorwin@gmail.com>
2026-02-25 10:43:12 +0000

    Removed VexFlow and now use images to show the stave.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,24 +1,23 @@
-// TO DO: REMOVE VEXFLOW
-
 let notes = {
-    "s": ["Cn4", "&#x2193;C", 261.63, "note-c"],
-    "e": ["C#4", "C#", 277.18, "note-c#"],
-    "d": ["Dn4", "D", 293.66, "note-d"],
-    "r": ["Eb4", "Eb", 311.13, "note-eb"],
-    "f": ["En4", "E", 329.63, "note-e"],
-    "g": ["Fn4", "F", 349.23, "note-f"],
-    "y": ["F#4", "F#", 369.99, "note-f#"],
-    "h": ["Gn4", "G", 392, "note-g"],
-    "u": ["Ab4", "Ab", 415.3, "note-ab"],
-    "j": ["An4", "A", 440, "note-a"],
-    "i": ["Bb4", "Bb", 466.16, "note-bb"],
-    "k": ["Bn4", "B", 493.88, "note-b"],
-    "l": ["Cn5", "&#x2191;C", 523.25, "note-C"]
+    "s": ["Cn4", "&#x2193;C", 261.63, "note-c", "c4-natural.png"],
+    "e": ["C#4", "C#", 277.18, "note-c#", "c4-sharp.png"],
+    "d": ["Dn4", "D", 293.66, "note-d", "d4-natural.png"],
+    "r": ["Eb4", "Eb", 311.13, "note-eb", "e4-flat.png"],
+    "f": ["En4", "E", 329.63, "note-e", "e4-natural.png"],
+    "g": ["Fn4", "F", 349.23, "note-f", "f4-natural.png"],
+    "y": ["F#4", "F#", 369.99, "note-f#", "f4-sharp.png"],
+    "h": ["Gn4", "G", 392, "note-g", "g4-natural.png"],
+    "u": ["Ab4", "Ab", 415.3, "note-ab", "a4-flat.png"],
+    "j": ["An4", "A", 440, "note-a", "a4-natural.png"],
+    "i": ["Bb4", "Bb", 466.16, "note-bb", "b4-flat.png"],
+    "k": ["Bn4", "B", 493.88, "note-b", "b4-natural.png"],
+    "l": ["Cn5", "&#x2191;C", 523.25, "note-C", "c5-natural.png"]
 };
 const NOTE_NAME = 0;
 const NOTE_TEXT = 1;
 const NOTE_FREQ = 2;
 const NOTE_VAR = 3;
+const NOTE_FILE = 4;
 
 const ctx = new AudioContext();
 const osc = ctx.createOscillator();
@@ -30,16 +29,12 @@ gain.gain.value = 0;
 osc.connect(gain).connect(ctx.destination);
 osc.start();
 
-// Vexflow constants
-let factory;
-let score;
-let system;
-
 let randomNote = ""
 let randomNotes = [];
 let currentNote = 0;
 
 let output = document.getElementById("notes");
+let stave = document.getElementById("stave");
 let successText = document.getElementById("success");
 
 let selected_key = "";
@@ -57,29 +52,20 @@ function startExercise() {
         output.innerHTML += notes[randomNote][NOTE_TEXT]
         if (i != 3) { output.innerHTML += ", " }
     }
-    drawStave();
+    loadStave()
 }
 
-function drawStave() {
-    document.getElementById("stave").innerHTML = "";
-
-    factory = new VexFlow.Factory({ renderer: { elementId: "stave", width: 500, height: 200 } });
-    score = factory.EasyScore();
-    system = factory.System({ width: 400 });
-
-    let note_output = "";
-    for (i = 0; i < 4; i++) {
-        note_output += notes[randomNotes[i]][NOTE_NAME];
-        if (i == 0) { note_output += "/q" }
-        if (i != 3) { note_output += ", " }
+function loadStave() {
+    while (stave.hasChildNodes()) {
+        stave.removeChild(stave.firstChild);
     }
-    system.addStave({
-        voices: [
-            score.voice(score.notes(note_output))
-        ],
-    }).addClef("treble").addTimeSignature("4/4");
-
-    factory.draw();
+    let img = document.createElement("img");
+    img.src = "assets/images/clef-sig.png";
+    stave.append(img.cloneNode(true));
+    for (i = 0; i < randomNotes.length; i++) {
+        img.src = "assets/images/" + notes[randomNotes[i]][NOTE_FILE];
+        stave.append(img.cloneNode(true));
+    }
 }
 
 function playNote(gainValue, frequencyValue = -1) {
@@ -93,25 +79,19 @@ function playNote(gainValue, frequencyValue = -1) {
 
 document.addEventListener("keydown", () => ctx.resume(), { once: true });
 
-// Vexflow
-window.addEventListener("load", function() {
-    VexFlow.loadFonts("Bravura", "Academico").then(() => {
-        VexFlow.setFonts("Bravura", "Academico");
-    });
-});
-
 document.addEventListener("keydown", (event) => {
     if (notes[event.key] != undefined && !keyPressed) {
         selected_key = event.key
         keyPressed = window.getComputedStyle(document.getElementById(notes[event.key][NOTE_VAR])).backgroundColor;
         document.getElementById(notes[event.key][NOTE_VAR]).style.backgroundColor = "#c0c0c0";
         playNote(1, notes[event.key][NOTE_FREQ]);
-
         if (failed == false && randomNotes[0] && currentNote < 4) {
             if (event.key == randomNotes[currentNote]) {
                 successText.innerHTML += "Success <br/>";
+                stave.children[currentNote + 1].style.backgroundColor = "#ddffdd";                
             } else {
                 successText.innerHTML += "Fail <br/>";
+                stave.children[currentNote + 1].style.backgroundColor = "#ffdddd"; 
                 successText.innerHTML += "<br/>You Lose!<br/>";
                 failed = true;
             }
