16d5f067336370dbf7a4c814973be8c162f763ff
KJOrwin <kjorwin@gmail.com>
2026-04-06 11:12:20 +0100

    Questions can now be imported. Next: allow for question navigation using the next button.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -33,6 +33,10 @@ osc.start();
 let randomNote = ""
 let randomNotes = [];
 let currentNote = 0;
+
+let current_question = [];
+let imported = false;
+let question_num = 1;
 let question_type = "";
 let question_notes = "";
 
@@ -47,6 +51,14 @@ let failed = false;
 
 let export_file = ["type,notes,include_accidentals,instant_fail\n"]
 
+let real_import = document.getElementById("real_import");
+let import_file = []
+const reader = new FileReader();
+reader.onload = () => {
+    import_file = reader.result.split("\n");
+    importFile();
+}
+
 let include_accidentals = document.getElementById("include_accidentals");
 let instant_fail = document.getElementById("instant_fail");
 let expimp_message = document.getElementById("expimp_message");
@@ -55,26 +67,36 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function staveExercise(blank) {
-    if (blank) {
-        question_type = "audio";
-    } else {
-        question_type = "stave";
+function staveExercise(type) {
+    if (imported == true) {
+        if (confirm("Creating a new question will override the questions that you have imported. Are you sure you want to proceed?")) {
+            imported = false;
+            include_accidentals.disabled = false;
+            instant_fail.disabled = false;
+        } else {
+            return;
+        }
     }
+    question_type = type;
     currentNote = 0;
     output.innerHTML = ""
     successText.innerHTML = ""
     failed = false;
     for (i = 0; i < 4; i++) {
         randomNote = Object.keys(notes)[Math.floor(Math.random() * Object.keys(notes).length)]
+        if (include_accidentals.checked == false) {
+            while (notes[randomNote][NOTE_COLOUR] == "black") {
+                randomNote = Object.keys(notes)[Math.floor(Math.random() * Object.keys(notes).length)]
+            }
+        }
         randomNotes[i] = randomNote;
         output.innerHTML += notes[randomNote][NOTE_TEXT]
         if (i != 3) { output.innerHTML += ", " }
     }
-    loadStave(blank)
+    loadStave()
 }
 
-function loadStave(blank) {
+function loadStave() {
     while (stave.hasChildNodes()) {
         stave.removeChild(stave.firstChild);
     }
@@ -82,11 +104,11 @@ function loadStave(blank) {
     img.className = "stave-elements";
     img.src = "assets/images/clef-sig.png";
     stave.append(img.cloneNode(true));
-    if (blank) {
-        actions.children[0].className = "col-sm-2"
-        actions.children[2].style.display = "block"
-        actions.children[3].style.display = "block"
-        actions.children[4].className = "col-sm-2"
+    if (question_type == "audio") {
+        changeActions(2, 0, 6, 1, 1, 0, 2);
+        if (imported) {
+            changeActions(1, 1, 6, 1, 1, 1, 1);
+        }
         img.src = "assets/images/blank-black.png";
         for (i = 0; i < 4; i++) {
             img.id = "n" + String(i + 1)
@@ -94,14 +116,56 @@ function loadStave(blank) {
         }
         return;
     }
-    actions.children[0].className = "col-sm-3"
-    actions.children[2].style.display = "none"
-    actions.children[3].style.display = "none"
-    actions.children[4].className = "col-sm-3"
+    changeActions(3, 0, 6, 0, 0, 0, 3);
+    if (imported) {
+        changeActions(2, 1, 6, 0, 0, 1, 2);
+    }
     for (i = 0; i < randomNotes.length; i++) {
         img.id = "n" + String(i + 1)
-        img.src = "assets/images/" + notes[randomNotes[i]][NOTE_FILE];
+        img.src = getFilePath(notes[randomNotes[i]][NOTE_FILE]);
         stave.append(img.cloneNode(true));
+    }
+}
+
+// This varaible is only used for error checking in the next function
+let totalColsActions = 0;
+
+function changeActions(...colWidth) {
+    /*
+        --- actions.children ---
+        0: left padding
+        1: question number (only used when a set of questions has been imported)
+        2: stave
+        3: audio button (only used for audio)
+        4: hint button (only used for audio)
+        5: next button (only used when a set of questions has been imported)
+        6: right padding
+    */
+    // Debug Error Checking
+    totalColsActions = 0;
+    for (i = 0; i < colWidth.length; i++) {
+        totalColsActions += colWidth[i];
+    }
+    if (totalColsActions != 12 || colWidth.length != 7) {
+        console.error("Action bar setup incorrectly");
+    }
+
+    // Adjust the column widths and display styling for the action bar
+    for (i = 0; i < actions.children.length; i++) {
+        actions.children[i].className = "col-sm-" + colWidth[i];
+        if (colWidth[i] == "0") {
+            actions.children[i].style.display = "none";
+        } else {
+            actions.children[i].style.display = "block";
+        }
+    }
+}
+
+function getFilePath(file_name) {
+    if (include_accidentals.checked) {
+        return "assets/images/" + file_name;
+    } else {
+        return "assets/images/" + file_name.slice(0, 2) + ".png";
     }
 }
 
@@ -122,7 +186,7 @@ async function playAudio() {
 function hint() {
     let note = document.getElementById("n" + String(currentNote + 1));    
     successText.innerHTML += "Hint <br/>";
-    note.src = "assets/images/" + notes[randomNotes[currentNote]][NOTE_FILE];
+    note.src = getFilePath(notes[randomNotes[currentNote]][NOTE_FILE]);
     stave.children[currentNote + 1].style.backgroundColor = "#ffeecc";
     currentNote++;
 }
@@ -130,21 +194,23 @@ function hint() {
 function pressKey(key) {
     let note = document.getElementById("n" + String(currentNote + 1));
     if (notes[key] != undefined && !keyPressed) {
-        selected_key = key
+        selected_key = key;
         keyPressed = window.getComputedStyle(document.getElementById(notes[key][NOTE_VAR])).backgroundColor;
         document.getElementById(notes[key][NOTE_VAR]).style.backgroundColor = "#c0c0c0";
         playNote(1, notes[key][NOTE_FREQ]);
         if (failed == false && randomNotes[0] && currentNote < 4) {
             if (key == randomNotes[currentNote]) {
                 successText.innerHTML += "Success <br/>";
-                note.src = "assets/images/" + notes[randomNotes[currentNote]][NOTE_FILE];
+                note.src = getFilePath(notes[randomNotes[currentNote]][NOTE_FILE]);
                 stave.children[currentNote + 1].style.backgroundColor = "#ddffdd";
             } else {
                 successText.innerHTML += "Fail <br/>";
-                note.src = "assets/images/" + notes[randomNotes[currentNote]][NOTE_FILE];
+                note.src = getFilePath(notes[randomNotes[currentNote]][NOTE_FILE]);
                 stave.children[currentNote + 1].style.backgroundColor = "#ffdddd";
                 successText.innerHTML += "<br/>You Lose!<br/>";
-                failed = true;
+                if (instant_fail.checked == true) {
+                    failed = true;
+                }
             }
 
             if (currentNote == 3 && failed == false) {
@@ -188,7 +254,15 @@ function exportFile() {
 }
 
 function importFile() {
-    return;
+    imported = true;
+    current_question = import_file[question_num].split(",")
+    question_type = current_question[0];
+    randomNotes = current_question[1].split("/");
+    include_accidentals.checked = JSON.parse(current_question[2]);
+    instant_fail.checked = JSON.parse(current_question[3]);
+    include_accidentals.disabled = true;
+    instant_fail.disabled = true;
+    loadStave();
 }
 
 document.addEventListener("keydown", () => ctx.resume(), { once: true });
@@ -207,3 +281,27 @@ document.addEventListener("keyup", (event) => {
         keyPressed = "";
     }
 });
+
+real_import.addEventListener("click", () => {
+    real_import.value = "";
+})
+
+real_import.addEventListener("change", () => {
+    reader.readAsText(real_import.files[0]);
+});
+
+include_accidentals.addEventListener("change", () => {
+    if (include_accidentals.checked) {
+        include_accidentals.title = "Questions will include sharps and flats"
+    } else {
+        include_accidentals.title = "Questions will not include sharps and flats"
+    }
+});
+
+instant_fail.addEventListener("change", () => {
+    if (instant_fail.checked) {
+        instant_fail.title = "Question will end when you make a mistake"
+    } else {
+        instant_fail.title = "Question will continue when you make a mistake"
+    }
+})
